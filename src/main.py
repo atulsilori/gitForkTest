3cd8ba255b33f3f2608e80f7cdca4f01b7b029bd
atulsilori <atul.silori@gmail.com>
2021-06-14 00:40:25 +0530

    created multiply function

--- src/main.py
+++ src/main.py
@@ -3,7 +3,14 @@ def add(num1, num2):
     number2 = num2
     return number1+number2
 
+def multiply(num1, num2):
+    number1 = num1
+    number2 = num2
+    return number1*number2    
+
 num1 = int(input("enter number1 >> "))
 num2 = int(input("enter number2 >> "))
 Sum = add(num1, num2)
-print(Sum)
+mult = multiply(num1, num2)
+print("sum is - ", Sum)
+print("multiplication is - ", mult)
